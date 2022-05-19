d6c85a18488195533a72fd73cfb95870c49faaa6
annvin <annvinvincent@gmail.com>
2022-05-19 21:22:44 +0530

    commit

--- cypress/integration/2-advanced-examples/newtest.js
+++ cypress/integration/2-advanced-examples/newtest.js
@@ -1,10 +1,11 @@
 
 
+
 describe('My first test', function()
 
 
 {
-    it('Verify if a user will be able to log in with a valid username and valid password', function()
+   it('Verify if a user will be able to log in with a valid username and valid password', function()
     {
         cy.visit('https://testvillage.letsventure.com/')
         cy.get('.signin').click()
@@ -205,9 +206,121 @@ describe('My first test', function()
                 return false
             })
         })
- })
-
+       it('Verify the messages for invalid logins.', function()
+        {
+            cy.fixture('credentials.json').then((credentials) => {
+                var em = credentials.email
+                var pass = credentials.password
+                var em1 = credentials.email1
+                
+                
+//valid email and invalid password
+            
+            cy.visit('https://testvillage.letsventure.com/signin')
+            cy.get('.signin').click()
+            cy.wait(2000)
+            cy.get('#email').type(credentials.email, {force:true})
+            cy.get('#password').type(credentials.password, {force:true})
+            cy.get('.btn').click({force:true})
+            cy.wait(4000)
+            cy.get('.error_notify').should('have.text','Please re-enter your password. The password you entered is incorrect. Forgot your password? Request a new one.')
+            //invalid email and invalid password
+            cy.visit('https://testvillage.letsventure.com/signin')
+            cy.get('.signin').click()
+            cy.wait(2000)
+            cy.get('#email').type(credentials.email1, {force:true})
+            cy.get('#password').type(credentials.password, {force:true})
+            cy.get('.btn').click({force:true})
+            cy.wait(4000)
+            cy.get('.error_notify').should('have.text','The email or password you entered is incorrect')
+        })
+           
     
+            Cypress.on('uncaught:exception', (err, runnable) => {
+                //returning false here prevents Cypress from
+                 //failing the test
+                return false
+            
+        })
+ })
+ it('Verify if the password is visible to the user after clicking on the show password button.', function()
+        {
+            cy.fixture('credentials.json').then((credentials) => {
+                var em = credentials.email
+                var pass1 = credentials.password1
+                
+                
+
+            
+            cy.visit('https://testvillage.letsventure.com/signin')
+            cy.get('.signin').click()
+            cy.wait(2000)
+            cy.get('#email').type(credentials.email, {force:true})
+            cy.get('#password').type(credentials.password1, {force:true})
+            cy.get('.field-icon > .fa').click({force:true})
+            cy.get('#password').should('have.attr', 'type', 'text')
+           
+            cy.get('.btn').click({force:true})
+            cy.wait(4000)
+            
+        })
+           
+    
+            Cypress.on('uncaught:exception', (err, runnable) => {
+                //returning false here prevents Cypress from
+                 //failing the test
+                return false
+            
+        })
+ })
+ it('Verify if the ‘Enter’ key of the keyboard is working correctly on the login page.', function()
+        {
+            cy.fixture('credentials.json').then((credentials) => {
+                var em = credentials.email
+                var pass1 = credentials.password1
+            cy.visit('https://testvillage.letsventure.com/signin')
+            cy.get('.signin').click()
+            cy.wait(2000)
+            cy.get('#email').type(credentials.email, {force:true})
+            cy.get('#password').type(credentials.password1 , {force:true})
+            cy.get('.custom_sign_in').find('#sign_in_submit').type(`{enter}`,{force:true})
+            
+            cy.wait(4000)
+            
+        })
+           
+    
+            Cypress.on('uncaught:exception', (err, runnable) => {
+                //returning false here prevents Cypress from
+                 //failing the test
+                return false
+            
+        })
+    })
+    it('Verify if the data in the password field is visible as bullet signs.', function()
+        {
+            cy.fixture('credentials.json').then((credentials) => {
+                var em = credentials.email
+                var pass1 = credentials.password1
+            cy.visit('https://testvillage.letsventure.com/signin')
+            cy.get('.signin').click()
+            cy.wait(2000)
+            cy.get('#email').type(credentials.email, {force:true})
+            cy.get('#password').type(credentials.password1, {force:true})
+            cy.get('#password').should('have.attr', 'type', 'password')
+            cy.wait(4000)
+            
+        })
+           
+    
+            Cypress.on('uncaught:exception', (err, runnable) => {
+                //returning false here prevents Cypress from
+                 //failing the test
+                return false
+            
+        })
+    })
+})
  
 
 //{
